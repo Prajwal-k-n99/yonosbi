04e854e7b10417c1228deee965ab38d121e175e1
Prajwal K N <prajwalkpura99@gmail.com>
2022-07-27 23:37:37 +0530

    yonosbi.java

--- src/com/company/yonosbi.java
+++ src/com/company/yonosbi.java
@@ -1,36 +1,78 @@
 package com.company;
 
-  class yonosbi {
-     String name;
-     long phno;
-     long accno;
-     double balance = 0;
-     double credit;
-     double debit;
-  double balance(){
-      return balance = balance + credit - debit ;
+import java.util.Calendar;
+import java.util.Scanner;
+
+ class Profile {
+      String name;
+      long phno;
+      long accno;
+
+      void details() {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the name");
+          name = sc.nextLine();
+          System.out.println("Enter the phone number");
+          phno = sc.nextLong();
+          System.out.println("Enter the account number");
+          accno = sc.nextInt();
+      }
   }
-  void details(){
-      System.out.println(name);
-      System.out.println(phno);
-      System.out.println(accno);
+  class BalanceCheck {
+      float balance = 0;
+      float credit;
+      float debit;
+      int upino;
+
+      float Balance() {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the upi no");
+          upino = sc.nextInt();
+           return balance = balance + credit - debit;
+      }
+
   }
-  double credit(){
-      return credit;
+  class Transactions{
+      float credit;
+      float debit;
+      String name;
+      float balance;
+  void Dedit(){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Search the name/phone number");
+      name = sc.nextLine();
+      System.out.println("Enter the debit amount");
+      debit = sc.nextFloat();
+      System.out.println("Debited amount is " + debit);
   }
-  double debit(){
-      return debit;
-  }
+  void Credit(){
+    Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the credit amount");
+      credit = sc.nextFloat();
+      System.out.println("Credited amount is "+ credit);
 }
-class bank{
+  void Transactions(){
+      int n=20;
+      balance =  credit - debit;
+      for (int i = 1;i<n;i++){
+      System.out.println(credit);
+      System.out.println(debit);
+      System.out.println(balance);
+  }
+  }
+  }
+class account{
     public static void main(String[] args) {
-        yonosbi myyono = new yonosbi();
-        myyono.name = "Prajwal";
-        myyono.balance() ;
-        myyono.phno = 702629235;
-        myyono.accno = 24586314;
-        myyono.debit();
-        myyono.credit();
+        Profile profile = new Profile();
+        BalanceCheck bc = new BalanceCheck();
+        Transactions trs = new Transactions();
+        Calendar c = Calendar.getInstance();
+        profile.details();
+        bc.Balance();
+        trs.Credit();
+        trs.Dedit();
+        trs.Transactions();
+        System.out.println(c.getTime());
     }
 
 }
